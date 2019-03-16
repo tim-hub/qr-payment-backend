3f64e3d40c369c339a2d319c6a962bea999465d3
tim-hub <tim_w@outlook.com>
2019-03-16 20:51:00 +1300

    add cors

--- app.py
+++ app.py
@@ -2,12 +2,14 @@ import os
 from flask_api import FlaskAPI
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
+from flask_cors import CORS
 
 app = FlaskAPI(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
+CORS(app)
 
 from api import *
 
