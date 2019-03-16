90fcc34cefb636194f3ded3fd00734b2dd1f65e3
tim-hub <tim_w@outlook.com>
2019-03-16 16:02:00 +1300

    update production config

--- config.py
+++ config.py
@@ -1,22 +1,19 @@
 import os
-from dotenv import load_dotenv
-load_dotenv()
-
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     DEBUG = False
     TESTING = False
     CSRF_ENABLED = True
-    SECRET_KEY = os.getenv("secret")
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
+    SECRET_KEY = os.environ["secret"]
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 
 
 class ProductionConfig(Config):
     DEBUG = False
     # the product env
     SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
-    SECRET_KEY = SQLALCHEMY_DATABASE_URI + os.getenv("secret")
+    SECRET_KEY = SQLALCHEMY_DATABASE_URI + os.environ["secret"]
 
 class StagingConfig(Config):
     DEVELOPMENT = True
